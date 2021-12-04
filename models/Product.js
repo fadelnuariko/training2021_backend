da2d39491531771f5a6edc4d64b07a43386b5b7f
fadelnuariko <fadelnuariko@gmail.com>
2021-12-04 16:37:30 +0800

    delete unused comment

--- models/Product.js
+++ models/Product.js
@@ -1,24 +1,18 @@
-// import sequelize 
 import { Sequelize } from "sequelize";
-// import connection 
+
 import db from "../config/database.js";
 
-// init DataTypes
 const { DataTypes } = Sequelize;
 
-// Define schema
 const Product = db.define('products', {
-  // Define attributes
-  title: {
-    type: DataTypes.STRING
-  },
-  price: {
-    type: DataTypes.DOUBLE
-  }
-},{
-  // Freeze Table Name
-  freezeTableName: true
+    title: {
+        type: DataTypes.STRING
+    },
+    price: {
+        type: DataTypes.DOUBLE
+    }
+}, {
+    freezeTableName: true
 });
 
-// Export model Product
 export default Product;
